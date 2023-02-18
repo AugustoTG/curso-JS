f5da03c4c9a8a48fd8a6ed1c95ab9899cadc3897
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-18 15:51:09 -0300

    update index.js

--- conceitos/aula 18/index.js
+++ conceitos/aula 18/index.js
@@ -1,6 +1,7 @@
 
 function dados (){
     const formPessoa = document.querySelector(".formulario");
+    const pessoa = [];
 
         //formPessoa.onsubmit = function (evento) {
         //evento.preventDefault();
@@ -14,8 +15,15 @@ function dados (){
             const sobrenome = formPessoa.querySelector('#sobrenome');
             const peso= formPessoa.querySelector('#peso');
             const altura = formPessoa.querySelector('#altura');
+            const resposta = document.querySelector('.resultado');
+            pessoa[0] = nome.value;
+            pessoa[1] = sobrenome.value;
+            pessoa[2] = peso.value;
+            pessoa[3] = altura.value;
+            console.log(pessoa)
+            resposta.innerHTML += `<p>Seu nome é:${pessoa[0]} ${pessoa[1]}, seu pesso: ${pessoa[2]}, sua alura:${pessoa[3]}</p>`;
         }
+       
         formPessoa.addEventListener('submit', recebeEventoForm);
 }
-
 dados();
