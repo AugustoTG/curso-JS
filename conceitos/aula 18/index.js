362e1b4fe28f2bca0ac1cceba0485e3d587a9c07
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-18 12:20:19 -0300

    update index.js and index.html

--- conceitos/aula 18/index.js
+++ conceitos/aula 18/index.js
@@ -1,2 +1,21 @@
 
-const pessoa  = ''
+function dados (){
+    const formPessoa = document.querySelector(".formulario");
+
+        //formPessoa.onsubmit = function (evento) {
+        //evento.preventDefault();
+        //alert(1);
+        //console.log('Foi enviado!');
+        //}
+
+        function recebeEventoForm (evento) {
+            evento.preventDefault();
+            const nome = formPessoa.querySelector('#nome');
+            const sobrenome = formPessoa.querySelector('#sobrenome');
+            const peso= formPessoa.querySelector('#peso');
+            const altura = formPessoa.querySelector('#altura');
+        }
+        formPessoa.addEventListener('submit', recebeEventoForm);
+}
+
+dados();
