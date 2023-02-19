c3aee037861302efb430ff6495cd1d77515ee1a8
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-19 14:29:49 -0300

    Create operadores.js

---
+++ logica de programação/aula 021/operadores.js
@@ -0,0 +1,12 @@
+/*
+operadores de computação 
+> maior que    / resposta boolean (true or false)
+>= maior que ou igual a  / resposta boolean (true or false)
+< menor que    / resposta boolean (true or false)
+<= menor que ou igual a  / resposta boolean (true or false)
+== igualdade  (valor)   / resposta boolean (true or false)
+=== igualdade restrita (valor e tipo)   / resposta boolean (true or false)
+!= diferentre (valor)  / resposta boolean (true or false)
+!== diferente restrito (valor e tipo)  / resposta boolean (true or false)
+*/
+console.log(10>5);
