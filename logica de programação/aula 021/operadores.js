1ddd05c64a686504d0d9fcb0b5c66a9cde6a761f
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-19 15:35:43 -0300

    Update operadores.js

--- logica de programação/aula 021/operadores.js
+++ logica de programação/aula 021/operadores.js
@@ -9,4 +9,18 @@ operadores de computação
 != diferentre (valor)  / resposta boolean (true or false)
 !== diferente restrito (valor e tipo)  / resposta boolean (true or false)
 */
-console.log(10>5);
+
+const num1 =10; // number 
+const num2 = '10'; // string
+const comp = num1 === num2;
+console.log(comp);
+
+/*
+Operadores lógicos 
+&& -> AND -> E   // o operador && precisa ser verdadeiro e verdadeiro para retornar true caso um deles não seja o operador retornará falso 
+|| -> or -> ou   // o operador || precisa ser falso e falso para retornar falso caso um deles não seja o operador retornará verdadeiro 
+! -> NOT -> NÂo // negação inverte os valores 
+*/
+
+let e = console.log(true && true); // o operador && precisa ser verdadeiro e verdadeiro para retornar true caso um deles não seja o operador retornará falso 
+let ou = console.log(false || false); // o operador || precisa ser falso e falso para retornar falso caso um deles não seja o operador retornará verdadeiro 
