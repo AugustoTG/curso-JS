3d7c9b178346c20445cecbc946a573974b546ae3
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-21 17:58:41 -0300

    update main.js

--- logica de programação/exercicio imc/assets/js/main.js
+++ logica de programação/exercicio imc/assets/js/main.js
@@ -1,28 +1,70 @@
 const form = document.querySelector('#formulario');
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    setResultado ('ola mundo')
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    if(!peso || !altura){
+        setResultado ('Peso ou Alturta invalido!', false);
+        p.classList.add('ColorRed');
+        return;
+    }
+    const imc = getImc(peso, altura);
+    console.log(imc)
 });
+function criarP (){
+    const p = document.createElement('p');
+    return p;
+}
+function getImc(peso, altura){
+    const imc = peso / altura **2;
+    return imc.toFixed(2);
+}
+function getNivelImc (imc){
+    const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+    if(imc >= 39.9){
 
-function setResultado(){
+    }else if(imc >= 34.9){
+
+    }else if(imc >= 29.9){
+        
+    }else if(imc >= 24.9){
+        
+    }else if(imc >= 18.5){
+        
+    }
+}
+function setResultado(msg, isValid){
+    p.classList.add('paragrafoResultado');
     const result = document.querySelector('#result');
-    const peso = document.querySelector('#peso');
-    const altura = document.querySelector('#altura');
-    const pesoN = peso.value;
-    const alturaN = altura.value;
-    const resultado = pesoN/alturaN ** 2;
     result.innerHTML = '';
+    const p = criarP();
+    p.innerHTML = msg;
+    result.appendChild(p);
+    
+    /*const resultado = (peso/altura ** 2)*10000;
     if(resultado < 18.5){
-        const p = document.createElement('p');
-        p.classList.add('paragrafoResultado'),
+        let p = criarP();
         p.innerHTML = `O seu IMC é ${resultado}, você esta a baixo do peso!`;
         result.appendChild(p);
-    }else if(resultado > 18.5 && resultado < 24,9){
-        const p = document.createElement('p');
-        p.classList.add('paragrafoResultado'),
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a com o peso normal!`;;
+    }else if(resultado > 18.5 && resultado < 24.9){
+        let p = criarP();
+        p.innerHTML = `O seu IMC é ${resultado}, você esta a com o peso normal!`;
         result.appendChild(p);
-    }
+    }else if(resultado >= 25 && resultado < 29.9){
+        let p = criarP();
+        p.innerHTML = `O seu IMC é ${resultado}, você esta a com sobrepeso!`;
+    }else if(resultado >= 30 && resultado < 34.9){
+        let p = criarP();
+        p.innerHTML = `O seu IMC é ${resultado}, você esta a com obesidade grau 1!`;
+    }else if (resultado >= 35 && resultado < 39.9){
+        let p = criarP();
+        p.innerHTML = `O seu IMC é ${resultado}, você esta a com obesidade grau 2!`;
+    }else{
+        let p = criarP();
+        p.innerHTML = `O seu IMC é ${resultado}, você esta a com obesidade grau 3!`;
+    }*/
     
 }
 
