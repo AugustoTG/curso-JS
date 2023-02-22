ca825ed69e7fe32e71b227ea34ffb21271ec33e5
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-22 12:02:55 -0300

    update main.js

--- logica de programação/exercicio imc/assets/js/main.js
+++ logica de programação/exercicio imc/assets/js/main.js
@@ -7,11 +7,12 @@ form.addEventListener('submit', function(e){
     const altura = Number(inputAltura.value);
     if(!peso || !altura){
         setResultado ('Peso ou Alturta invalido!', false);
-        p.classList.add('ColorRed');
         return;
     }
     const imc = getImc(peso, altura);
-    console.log(imc)
+    const nivelImc = getNivelImc(imc);
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+    setResultado(msg, true);
 });
 function criarP (){
     const p = document.createElement('p');
@@ -23,48 +24,25 @@ function getImc(peso, altura){
 }
 function getNivelImc (imc){
     const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
-    if(imc >= 39.9){
-
-    }else if(imc >= 34.9){
-
-    }else if(imc >= 29.9){
-        
-    }else if(imc >= 24.9){
-        
-    }else if(imc >= 18.5){
-        
-    }
+    if(imc >= 39.9) return nivel[5];
+    if(imc >= 34.9) return nivel[4];
+    if(imc >= 29.9) return nivel [3];
+    if(imc >= 24.9) return nivel [2];
+    if(imc >= 18.5) return nivel [1];
+    if(imc < 18.5) return nivel [0];
+    
 }
 function setResultado(msg, isValid){
-    p.classList.add('paragrafoResultado');
     const result = document.querySelector('#result');
     result.innerHTML = '';
     const p = criarP();
+    if (isValid){
+        p.classList.add('paragrafoResultado');
+    }else{
+        p.classList.add('colorRed')
+    }
     p.innerHTML = msg;
     result.appendChild(p);
-    
-    /*const resultado = (peso/altura ** 2)*10000;
-    if(resultado < 18.5){
-        let p = criarP();
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a baixo do peso!`;
-        result.appendChild(p);
-    }else if(resultado > 18.5 && resultado < 24.9){
-        let p = criarP();
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a com o peso normal!`;
-        result.appendChild(p);
-    }else if(resultado >= 25 && resultado < 29.9){
-        let p = criarP();
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a com sobrepeso!`;
-    }else if(resultado >= 30 && resultado < 34.9){
-        let p = criarP();
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a com obesidade grau 1!`;
-    }else if (resultado >= 35 && resultado < 39.9){
-        let p = criarP();
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a com obesidade grau 2!`;
-    }else{
-        let p = criarP();
-        p.innerHTML = `O seu IMC é ${resultado}, você esta a com obesidade grau 3!`;
-    }*/
     
 }
 
