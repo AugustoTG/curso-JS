1f40e673753e6c53afbe796aaf259fe69a3eff76
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-23 22:41:58 -0300

    Create estrutura de repeticao for.js

---
+++ logica de programação/aula 028/estrutura de repeticao for.js
@@ -0,0 +1,19 @@
+/*console.log('linha0');
+console.log('linha1');
+console.log('linha2');
+console.log('linha3');
+console.log('linha4');
+console.log('linha5');*/
+// i -> index comumente conhecido
+//for(let i = 0; i <= 5; i++){
+//console.log(`linha${i}`);
+//}
+//for(let i = 0; i <= 10; i++){
+//    const par = i % 2 === 0 ? 'par': 'ímpar';
+//    console.log(i, par)
+//    }
+
+const fruta = [ 'maçã', 'pêra', 'banana', 'uva', 'morango', 'kiwi'];
+for(let i = 0; i < fruta.length; i++){
+    console.log(`índice ${i}`, fruta[i]);
+    }
