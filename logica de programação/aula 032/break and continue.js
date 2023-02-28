1169c7eadfc07c0b079953d358e4bcb6860d2a73
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-28 20:29:31 -0300

    Create break and continue.js

---
+++ logica de programação/aula 032/break and continue.js
@@ -0,0 +1,13 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let numero of numeros){
+    if(numero === 2 || numero === 5){
+        console.log('pulei')
+        continue;
+    }
+    console.log(numero);
+    if(numero === 7){
+        console.log('7 encontrado');
+        break
+    }
+}
