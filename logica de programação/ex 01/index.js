fb7c8d4bc2551a147c2eb2bc5866a8d47c71affa
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-02-28 22:39:32 -0300

    Create index.js

---
+++ logica de programação/ex 01/index.js
@@ -0,0 +1,23 @@
+
+/*function number( n1, n2){
+    if(n1 > n2){
+        return n1;
+    }else{
+        return n2;
+    }
+} */
+/*function number( n1, n2){
+    if(n1 > n2) return n1;
+        return n2;
+} */
+/*function number( n1, n2){
+    return n1 > n2 ? n1 : n2;
+} */
+
+/*
+const number = (n1, n2) => {
+    return n1 > n2 ? n1 : n2;
+}*/
+const number = (n1, n2) => n1 > n2 ? n1 : n2;
+const max = number(10, 5)
+console.log(max);
