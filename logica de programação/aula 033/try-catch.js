cf98738f1ce854931b6252d84599986a883c824f
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-05 16:42:35 -0300

    Update try-catch.js

--- logica de programação/aula 033/try-catch.js
+++ logica de programação/aula 033/try-catch.js
@@ -29,9 +29,26 @@ try{
 }
 
 function retornaHora(data){
-    if(!(data && data instanceof Date)){
+    if(data && !(data instanceof Date)){
         throw new TypeError('Esperando instância de Date.')
-
     }
-
+    if(!data){
+        data = new Date ();
+    }
+    return data.toLocaleTimeString('pt-BR', {
+        hour : '2-digit',
+        minute : '2-digit',
+        second : '2-digit',
+        hour12 : false
+        });
+}
+try{
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora(data);
+    console.log(hora);
+} catch(err){ 
+    //tratar erro
+    //console.log(err);
+} finally {
+    console.log("tenha um bom dia")
 }
