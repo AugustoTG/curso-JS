2d4a5b717cbc16ff288ef7697b8f09a91d97d8aa
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-18 22:20:34 -0300

    Update main.js

--- 02 Funções/exercicio calculadora/assets/js/main.js
+++ 02 Funções/exercicio calculadora/assets/js/main.js
@@ -1,6 +1,19 @@
-(function(){
+function criaCalculadora(){
+return {
+    display: document.querySelector('.display'),
+
+
+
+    inicia() {
     
+    },
 
-
-
-})()
+    cliqueBotos(){
+        document.addEventListener('click' function(e){
+            
+        })
+    },
+}
+}
+const calculadora = criaCalculadora();
+calculadora.inicia()
