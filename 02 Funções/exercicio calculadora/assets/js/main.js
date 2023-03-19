4a7e78fec861e41cbb645e477a9c8cdc5d4428b1
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-19 10:28:53 -0300

    Update main.js

--- 02 Funções/exercicio calculadora/assets/js/main.js
+++ 02 Funções/exercicio calculadora/assets/js/main.js
@@ -5,13 +5,21 @@ return {
 
 
     inicia() {
-    
+        this.cliqueBotos();
     },
 
     cliqueBotos(){
-        document.addEventListener('click' function(e){
+        document.addEventListener('click', function(e){
+           const el = e.target;
             
-        })
+           if(el.classList.contains('btn btn-num')){
+            this.btnParaDisplay(el.innerText);
+           }
+        }.bind(this))
+    },
+
+    btnParaDisplay(valor){
+        this.display.value += valor;
     },
 }
 }
