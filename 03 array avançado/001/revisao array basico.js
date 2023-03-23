15bde01a47cde01c50b2cd15ce4a3269fed73a6a
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-23 15:39:34 -0300

    Update revisao array basico.js

--- 03 array avançado/001/revisao array basico.js
+++ 03 array avançado/001/revisao array basico.js
@@ -6,7 +6,7 @@ names[2] = 'Marcilene'
 
 console.log(names);
 
-console.log(nomes.length);// .length é um atributo entao não se usa parentes, só se usa parentes em metodos 
+console.log(names.length);// .length é um atributo entao não se usa parentes, só se usa parentes em metodos 
 
 //const removidos = nomes.pop();
 //const removido = nomes.shift();
@@ -15,16 +15,16 @@ console.log(nomes.length);// .length é um atributo entao não se usa parentes, 
 
 console.log(names)
 
-nomes.push('João');
-nomes.unshift('João');
+names.push('João');
+names.unshift('João');
 
 
-const novo = nomes.slice(1, 3); // divide o array pegando os indices 1 e 2 no caso 
+const novo = names.slice(1, 3); // divide o array pegando os indices 1 e 2 no caso 
 
 
 // const nome2 = "Augusto Tavares Gaspar";
 // const nomes2 = nome2.split ('') // trasforma a string em um array
 
-const nome2 = ["Augusto Tavares Gaspar"];
-const nomes2 = nome2.join (' ')
+const name2 = ["Augusto Tavares Gaspar"];
+const names2 = name2.join (' ')
 
