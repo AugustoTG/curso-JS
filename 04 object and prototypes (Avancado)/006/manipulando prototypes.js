0e8d156f8b21d573f100a821ae02d4c463768c83
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-28 16:30:58 -0300

    Update manipulando prototypes.js

---
+++ 04 object and prototypes (Avancado)/006/manipulando prototypes.js
@@ -0,0 +1,11 @@
+// new Object -> Object.prototype
+const objA = {
+    chaveA: 'A'
+    // ___proto___: Object.prototype
+};
+
+// new Object -> Object.prototype
+const objB = {
+    chaveB: 'B'
+    // ___proto___: Object.prototype
+};
