4f9722882ae97f4d27f14d42f25ec690f7987abf
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-03-28 17:03:05 -0300

    Update manipulando prototypes.js

--- 04 object and prototypes (Avancado)/006/manipulando prototypes.js
+++ 04 object and prototypes (Avancado)/006/manipulando prototypes.js
@@ -7,5 +7,60 @@ const objA = {
 // new Object -> Object.prototype
 const objB = {
     chaveB: 'B'
-    // ___proto___: Object.prototype
+    // ___proto___: ObjA
 };
+
+
+const objC = new Object();
+objC.chaveC = 'C';
+
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+console.log(objB.chaveA)
+console.log(objC.chaveB)
+
+
+function Produto(nome, preco){
+this.nome = nome;
+this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual){
+    this.preco = this.preco- (this.preco * (percentual/100))
+}
+
+Produto.prototype.acrescimo = function(percentual){
+    this.preco = this.preco + (this.preco * (percentual/100))
+}
+
+const p1 = new Produto('cacto', 4);
+//p1.desconto(100);
+p1.acrescimo(100)
+console.log(p1)
+
+const p2 = {
+    nome: 'caneca',
+    preco: 5
+}
+Object.setPrototypeOf(p2, Produto.prototype)
+p2.desconto(10);
+console.log(p2)
+
+
+const p3 = Object.create(Produto.prototype, {
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42
+    },
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 52
+    }
+});
+p3.nome = 'urso'
+p3.acrescimo(5)
+console.log(p3)
