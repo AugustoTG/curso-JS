7e51bc514822069958bd01a2efc6962b076ef371
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-04-06 21:09:23 -0300

    Update polimorfismo.js

---
+++ 04 object and prototypes (Avancado)/008/polimorfismo.js
@@ -0,0 +1,52 @@
+// superclass
+function Conta(agencia, conta, saldo){
+    this.agencia = agencia;
+    this.conta = conta;
+    this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function(valor){
+    if(this.saldo < valor){
+        console.log(`saldo insuficiente ${this.saldo}`)
+        return;
+    }
+    this.saldo -= valor
+    this.verSaldo();
+};
+Conta.prototype.depositar = function(valor){
+    this.saldo += valor;
+    this.verSaldo();
+};
+Conta.prototype.verSaldo = function(){
+    console.log(`Ag/c:${this.agencia}/${this.conta}, Saldo: ${this.saldo.toFixed(2)}`);
+};
+
+function ContaCorrente (agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente; 
+
+ContaCorrente.prototype.sacar = function(valor){
+    if(valor > (this.saldo + this.limite) ){
+        console.log(`saldo insuficiente ${this.saldo}`)
+        return;
+    }
+    this.saldo -= valor
+    this.verSaldo();
+};
+
+function ContaPoupanca(agencia, conta, saldo){
+    Conta.call(this, agencia, conta, saldo);
+}
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaCorrente; 
+const cc = new ContaCorrente(11,22,0,100);
+cc.depositar(10);
+cc.sacar(90);
+
+const cp = new ContaPoupanca(12, 23, 30);
+cp.depositar(50);
+cp.sacar(90);
+
