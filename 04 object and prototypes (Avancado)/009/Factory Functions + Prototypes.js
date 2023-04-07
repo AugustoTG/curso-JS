977794cc4b2d474cf2b1c623c305874256d8b284
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-04-07 16:12:47 -0300

    Create Factory Functions + Prototypes.js

---
+++ 04 object and prototypes (Avancado)/009/Factory Functions + Prototypes.js
@@ -0,0 +1,31 @@
+function CriaPessoa(nome, sobrenome){
+    return Object.create(pessoaPrototype, {
+        nome: {value: nome},
+        sobrenome: {value: sobrenome},
+       
+    });
+}
+const falar = {
+    fala(){
+        console.log(`${this.nome} esta falando `)
+    }
+};
+
+const comer = {
+    comer(){
+        console.log(`${this.nome} esta comendo`)
+    }
+};
+
+const beber = {
+    beber(){
+        console.log(`${this.nome} esta bebendo `)
+    }
+};
+const pessoaPrototype = {...falar, ...comer, ...beber}
+
+const pessoa1 = CriaPessoa('Augusto', 'Tavares')
+console.log(pessoa1)
+
+const pessoa2 = CriaPessoa('Marina', 'Tavares')
+console.log(pessoa1)
