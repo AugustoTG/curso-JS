443bb478f548d488c342d867c665870dff0a96a0
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-04-22 20:22:13 -0300

    Update promises 2.js

--- 06 promises ajax fetch e async/001/promises 2.js
+++ 06 promises ajax fetch e async/001/promises 2.js
@@ -1,27 +1,37 @@
-
+function rand(min, max){
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random()* (max - min) + min)
+}
  function esperaAi(msg, tempo){
     return new Promise((resolve, reject) =>{
-        if(typeof msg !== "string"){
-            reject('Bad Value!')
-            return
-        }
         setTimeout(() =>{
+            if(typeof msg !== "string"){
+                reject('Bad Value!')
+                return
+            }
             resolve(msg);
         }, tempo)
     }) 
  }
 
  // promise.all promise.race promise.resolve promise.reject
- const promises = ['primeiro valor',
-    esperaAi('Promise 1', 3000),
-    esperaAi('Promise 2', 500),
-    esperaAi('Promise 3', 1000),
-    esperaAi(1000, 1000),
-    'outro valor'
+ const promises = [/*'primeiro valor',*/
+    esperaAi('Promise 1', rand(1,3)),
+    esperaAi('Promise 2', rand(1,3)),
+    esperaAi('Promise 3', rand(1,3)),
+    esperaAi(1000, rand(1,3)),
+    /*'outro valor'*/
 ];
 
-Promise.all(promises).then(function(valor){
-    console.log(valor)
-}).catch(function(erro){
-    console.log(erro);
-});
+// Promise.all(promises).then(function(valor){
+//     console.log(valor)
+// }).catch(function(erro){
+//     console.log(erro);
+// });
+
+// Promise.race(promises).then(function(valor){
+//     console.log(valor)
+// }).catch(function(erro){
+//     console.log(erro);
+// });
