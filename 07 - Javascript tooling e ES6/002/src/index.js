148ade47ba955213d9bea11f8be6294eb208d025
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-05-02 20:45:14 -0300

    update files

--- 07 - Javascript tooling e ES6/002/src/index.js
+++ 07 - Javascript tooling e ES6/002/src/index.js
@@ -1,6 +1,6 @@
 //podemos importar constantes com outro nome basta colocar  'as' ex: x as y
 
-import { nome as nome2, sobrenome, idade, soma} from './modulo1';
+import multiplica, { nome as nome2, sobrenome, idade, soma, Pessoa} from './modulo1';
 
 const nome = 'joão'
 console.log(nome2, sobrenome, idade)
@@ -8,3 +8,14 @@ console.log(nome2, sobrenome, idade)
 console.log(soma(5, 2))
 
 console.log(nome)
+
+
+const p1 = new Pessoa('Midiã', 'Gaspar');
+
+console.log(p1)
+
+// import modelo 2 - importa todos os itens 
+// import * as MeuModulo from './modulo1';
+
+console.log(multiplica(5,5))
+
--- 07 - Javascript tooling e ES6/002/src/modulo1.js
+++ 07 - Javascript tooling e ES6/002/src/modulo1.js
@@ -1,10 +1,31 @@
-const nome = 'Augusto';
-const sobrenome = 'Tavares'
-const idade = 30
+// const nome = 'Augusto';
+// const sobrenome = 'Tavares'
+// const idade = 30
 
-function soma(x, y){
+// function soma(x, y){
+//     return x + y;
+// }
+
+// //podemos exportar constantes com outro nome basta colocar  'as' ex: x as y
+
+// // export ex 1
+// export {nome, sobrenome, idade, soma}
+
+
+// ex 2 - pode-se exportar diretamente os elementos
+export const nome = 'Augusto';
+export const sobrenome = 'Tavares'
+export const idade = 30
+
+export function soma(x, y){
     return x + y;
 }
 
-//podemos exportar constantes com outro nome basta colocar  'as' ex: x as y
-export {nome, sobrenome, idade, soma}
+export class Pessoa {
+    constructor (nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }    
+}
+// export default exporta um elemento como padrão, não sendo necessario abrir chaves e pode-se colocar qualquer nome 
+export default (f, a) => f*a;
